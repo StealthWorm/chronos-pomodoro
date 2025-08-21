685a011e2e2f58fb8de1a9b1ce6397c999f7b254
Thierry Santos <ThierryPitela@hotmail.com>
2025-08-20 23:16:25 -0300

    feat: included basic routes for all pages necessary for the project

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,13 @@
+import styles from "./styles.module.css";
 
 export function Home() {
   return (
     <main>
-      <div className="timer-container">
-        <div className="timer-display">
+      <div className={styles.timerContainer}>
+        <div className={styles.timerDisplay}>
           <span>00:00:00</span>
         </div>
       </div>
     </main>
-
   )
 }
