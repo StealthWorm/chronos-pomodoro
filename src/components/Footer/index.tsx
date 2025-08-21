1e925d6b77492ea2246acfe92c8ff954abf1de81
Thierry Santos <ThierryPitela@hotmail.com>
2025-08-20 23:15:37 -0300

    refactor: moved Footer logic to its own component

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,20 @@
+import styles from "./styles.module.css";
+
+export function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <span>
+        <a
+          href="https://pt.wikipedia.org/wiki/T%C3%A9cnica_pomodoro"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Entenda a técnica Pomodoro 🍅
+        </a>
+      </span>
+      <p>
+        Chronos Pomodoro {new Date().getFullYear()} - 💚
+      </p>
+    </footer>
+  )
+}
