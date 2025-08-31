7521112650beb1b0c50c1a0dda4e09dc8e179ebf
Thierry Santos <ThierryPitela@hotmail.com>
2025-08-31 09:08:27 -0300

    reafctor: included basic form + adjustments in layout

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,29 +1,34 @@
-import { useEffect, useState } from "react";
+import { Container } from "../../components/Container";
+import { Countdown } from "../../components/Countdown";
+import { Input } from "../../components/Input";
 import styles from "./styles.module.css";
 
 export function Home() {
-  const [time, setTime] = useState<number>(10 * 60 * 1000); // 10 minutes
-
-  useEffect(() => {
-    const interval = setInterval(() => setTime(time - 10), 10);
-
-    return () => clearInterval(interval);
-  }, [time]);
-
-  const formatTime = (time: number) => {
-    const minutes = Math.floor(time / 60000);
-    const seconds = Math.floor((time % 60000) / 1000);
-    const milliseconds = Math.floor((time % 1000) / 10);
-    return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}:${milliseconds.toString().padStart(2, '0')}`;
-  }
-
   return (
-    <main>
-      <div className={styles.timerContainer}>
-        <div className={styles.timerDisplay}>
-          <span>{formatTime(time)}</span>
-        </div>
-      </div>
-    </main>
+    <>
+      <Container>
+        <Countdown />
+      </Container>
+      <Container>
+        <form className={styles.form} action="">
+          <Input label="Task" placeholder="Enter your task" id="task" type="text" />
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
+          </p>
+
+          <div className={styles.cycle}>
+            <p>Ciclos:</p>
+            <ul>
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
+          </div>
+
+          <button type="submit" className={styles.button}>Start</button>
+        </form>
+      </Container>
+    </>
   )
 }
