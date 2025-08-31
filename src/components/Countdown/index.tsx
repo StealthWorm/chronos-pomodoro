3c0460cf264846ceb6b4974bae5c20b683657cd3
Thierry Santos <ThierryPitela@hotmail.com>
2025-08-31 09:07:43 -0300

    feat: Countdown component creation

---
+++ src/components/Countdown/index.tsx
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import styles from "./styles.module.css";
+
+export function Countdown() {
+  const [time, setTime] = useState<number>(10 * 60 * 1000); // 10 minutes
+  const [isRunning, setIsRunning] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (isRunning) {
+      const interval = setInterval(() => {
+        if (time <= 0) {
+          setIsRunning(false);
+        }
+        setTime(time - 10)
+      }, 10);
+      return () => clearInterval(interval);
+    }
+  }, [time, isRunning]);
+
+  const formatTime = (time: number) => {
+    const minutes = Math.floor(time / 60000);
+    const seconds = Math.floor((time % 60000) / 1000);
+    return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+  }
+
+  const handleTimerStart = () => {
+    setIsRunning(true);
+  }
+
+  return (
+    <div className={styles.timerContainer} onClick={handleTimerStart}>
+      {formatTime(time)}
+    </div>
+  )
+}
