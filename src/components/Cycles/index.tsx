70c454b9ebc9405c13745e9e99527389dddc7431
Thierry Santos <ThierryPitela@hotmail.com>
2025-09-04 22:42:29 -0300

    feat: created Cycles component

---
+++ src/components/Cycles/index.tsx
@@ -0,0 +1,21 @@
+import styles from "./styles.module.css";
+
+export function Cycles() {
+  return (
+    <div className={styles.cycleContainer}>
+      <span>Ciclos:</span>
+
+      <ul className={styles.cycleList}>
+        <li className={`${styles.cycleListDot} ${styles.workTime}`}></li>
+        <li className={`${styles.cycleListDot} ${styles.shortBreakTime}`}></li>
+        <li className={`${styles.cycleListDot} ${styles.workTime}`}></li>
+        <li className={`${styles.cycleListDot} ${styles.stopTime}`}></li>
+        <li className={`${styles.cycleListDot} ${styles.shortBreakTime}`}></li>
+        <li className={`${styles.cycleListDot} ${styles.workTime}`}></li>
+        <li className={`${styles.cycleListDot} ${styles.shortBreakTime}`}></li>
+        <li className={`${styles.cycleListDot} ${styles.workTime}`}></li>
+        <li className={`${styles.cycleListDot} ${styles.longBreakTime}`}></li>
+      </ul>
+    </div>
+  )
+}
