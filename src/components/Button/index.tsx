fcfb0bb70a75a47d28a425edfa9a326e6bc1f1f9
Thierry Santos <ThierryPitela@hotmail.com>
2025-09-06 15:38:01 -0300

    feat: created Button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,14 @@
+import styles from './styles.module.css';
+
+type ButtonProps = {
+  icon: React.ReactNode;
+  color?: 'success' | 'warning' | 'error';
+} & React.ComponentProps<'button'>;
+
+export function Button({ icon, type = 'button', color = 'success', ...props }: ButtonProps) {
+  return (
+    <button type={type} className={`${styles.button} ${styles[color]}`} {...props}>
+      {icon}
+    </button>
+  )
+}
