b9ada4dee154ad33ef990a86097197abd3708dcc
Thierry Santos <ThierryPitela@hotmail.com>
2025-09-28 12:15:11 -0300

    refactor: moved countdown logic to the task context

--- src/components/Countdown/index.tsx
+++ src/components/Countdown/index.tsx
@@ -1,37 +1,14 @@
-import { useContext, useEffect, useState } from "react";
+
+
+import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
 import styles from "./styles.module.css";
-import { TasksContext } from "../../contexts";
 
 export function Countdown() {
-  const { secondsRemaining } = useContext(TasksContext);
-  const [time, setTime] = useState<number>(secondsRemaining); // 10 minutes
-  const [isRunning, setIsRunning] = useState<boolean>(false);
-
-  useEffect(() => {
-    if (isRunning) {
-      const interval = setInterval(() => {
-        if (time <= 0) {
-          setIsRunning(false);
-        }
-        setTime(time - 10)
-      }, 10);
-      return () => clearInterval(interval);
-    }
-  }, [time, isRunning]);
-
-  const formatTime = (time: number) => {
-    const minutes = Math.floor(time / 60000);
-    const seconds = Math.floor((time % 60000) / 1000);
-    return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
-  }
-
-  const handleTimerStart = () => {
-    setIsRunning(true);
-  }
+  const { state: { formattedSecondsRemaining } } = useTaskContext();
 
   return (
-    <div className={styles.timerContainer} onClick={handleTimerStart}>
-      {formatTime(time)}
+    <div className={styles.timerContainer}>
+      {formattedSecondsRemaining}
     </div>
   )
 }
