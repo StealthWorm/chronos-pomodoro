dc75714be95ce0dd046d96f5134cdf68190eae2c
Thierry Santos <ThierryPitela@hotmail.com>
2025-09-28 12:15:47 -0300

    feat: created simple form logic to dispatch a new cicle and start countdown

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -5,15 +5,23 @@ import { Countdown } from "../../components/Countdown";
 import { Cycles } from "../../components/Cycles";
 import { Input } from "../../components/Input";
 import styles from "./styles.module.css";
+import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
 
 export function Home() {
+  const { actionTimerStart, state } = useTaskContext();
+
+  const handleStartTask = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    actionTimerStart();
+  }
+
   return (
     <>
       <Container>
         <Countdown />
       </Container>
       <Container>
-        <form className={styles.form} action="">
+        <form className={styles.form} action="" onSubmit={(e) => handleStartTask(e)}>
           <Input label="Task" placeholder="Enter your task" id="task" type="text" />
 
           <p>
@@ -22,7 +30,14 @@ export function Home() {
 
           <Cycles />
 
-          <Button type="submit" icon={<PlayCircleIcon size={32} />} />
+          <Button
+            type="submit"
+            icon={state.currentCycle > 0
+              ? <StopCircleIcon size={32} />
+              : <PlayCircleIcon size={32} />
+            }
+            color={state.currentCycle > 0 ? "error" : "success"}
+          />
         </form>
       </Container>
     </>
