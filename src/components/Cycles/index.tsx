48e639345b1408018e8ce52e55452abde4dd9a0e
Thierry Santos <ThierryPitela@hotmail.com>
2025-10-13 21:16:36 -0300

    refactor: cycles component fix to handle context task list

--- src/components/Cycles/index.tsx
+++ src/components/Cycles/index.tsx
@@ -1,19 +1,31 @@
+import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
 import styles from "./styles.module.css";
 
 export function Cycles() {
+  const { state } = useTaskContext();
+
+  const cycleStep = Array.from({ length: state.tasks.length, }, (_, index) => state.tasks[index]);
+
+  const cycleDescription = {
+    focusDuration: "Periodo de Foco",
+    shortBreakDuration: "Intervalo Curto",
+    longBreakDuration: "Intervalo Longo",
+    stopTime: "Interrupção"
+  };
+
   return (
     <div className={styles.cycleContainer}>
       <span>Ciclos:</span>
 
       <ul className={styles.cycleList}>
-        <li className={`${styles.cycleListDot} ${styles.workTime}`}></li>
-        <li className={`${styles.cycleListDot} ${styles.shortBreakTime}`}></li>
-        <li className={`${styles.cycleListDot} ${styles.workTime}`}></li>
-        <li className={`${styles.cycleListDot} ${styles.stopTime}`}></li>
-        <li className={`${styles.cycleListDot} ${styles.shortBreakTime}`}></li>
-        <li className={`${styles.cycleListDot} ${styles.workTime}`}></li>
-        <li className={`${styles.cycleListDot} ${styles.shortBreakTime}`}></li>
-        <li className={`${styles.cycleListDot} ${styles.longBreakTime}`}></li>
+        {cycleStep.map((task) => (
+          <li
+            className={`${styles.cycleListDot} ${styles[task.type]} ${task.id === state.activeTask?.id ? styles.active : ''}`}
+            key={task.id}
+            title={cycleDescription[task.type]}
+            aria-label={`Indicador de Ciclo - ${cycleDescription[task.type]}`}
+          ></li>
+        ))}
       </ul>
     </div>
   )
