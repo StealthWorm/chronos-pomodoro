40d4ff0a6420b626e917f31c79dc3f9ff579c6d7
Thierry <thierrypitela@hotmail.com>
2025-11-01 15:31:16 -0300

    feat: updated worker to change the current time and send the value back to the main context

--- src/workers/timerWorker.js
+++ src/workers/timerWorker.js
@@ -1,3 +1,24 @@
+let isRunning = false;
+
 self.onmessage = function (event) {
-  console.log('WORKER RECEIVED ', event.data);
+  if (isRunning) return;
+
+  isRunning = true;
+
+  const { millisecondsRemaining, activeTask } = event.data;
+
+  const endDate = activeTask.startDate + millisecondsRemaining;
+  const now = Date.now();
+  let countDownMilliseconds = Math.ceil(endDate - now);
+
+  function tick() {
+    self.postMessage(countDownMilliseconds);
+
+    const now = Date.now();
+    countDownMilliseconds = Math.ceil(endDate - now);
+
+    setTimeout(tick, 1000);
+  }
+
+  tick();
 };
