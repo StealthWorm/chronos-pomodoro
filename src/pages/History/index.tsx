b51cc375c473eb4254736b2ddc481552fdc5625c
Thierry <thierrypitela@hotmail.com>
2025-11-18 21:30:03 -0300

    feat: configured History list with saved data

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -2,14 +2,28 @@ import { TrashIcon } from 'lucide-react'
 import { Container } from '../../components/Container';
 import { Heading } from '../../components/Heading';
 import { Button } from '../../components/Button';
+import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
+import { formatDate } from '../../utils/formatDate';
 import styles from './styles.module.css';
+import { formatTime } from '../../utils/formatTime';
 
 export function History() {
+  const { state } = useTaskContext();
+
   const taskStatusColor = {
     completed: 'success',
     interrupted: 'error',
     pending: 'warning',
   }
+
+  const taskTypeTranslation = {
+    focusDuration: 'Foco',
+    shortBreakDuration: 'Intervalo Curto',
+    longBreakDuration: 'Intervalo Longo',
+  }
+
+  const orderedTasks = state.tasks.sort((a, b) => new Date(b.startDate).getTime() - new Date(a.startDate).getTime());
+
   return (
     <div className={styles.historyContainer}>
       <Container>
@@ -40,16 +54,25 @@ export function History() {
             </thead>
 
             <tbody>
-              {Array.from({ length: 20 }).map((_, index) => {
+              {orderedTasks.map((task) => {
                 return (
-                  <tr key={index}>
-                    <td>Estudar</td>
-                    <td>25min</td>
-                    <td>20/04/2025 08:00</td>
+                  <tr key={task.id}>
+                    <td>{task.title}</td>
+                    <td>{formatTime(task.duration)}</td>
+                    <td>{formatDate(task.startDate)}</td>
+                    {task.interruptDate &&
+                      <td >
+                        <span className={styles[taskStatusColor.interrupted]}>Interrompido</span>
+                      </td>}
+                    {task.endDate && <td>
+                      <span className={styles[taskStatusColor.completed]}>Completado</span>
+                    </td>}
+                    {!task.interruptDate && !task.endDate && <td>
+                      <span className={styles[taskStatusColor.pending]}>Pendente</span>
+                    </td>}
                     <td>
-                      <span className={styles[taskStatusColor.completed]}>Completa</span>
+                      <span>{taskTypeTranslation[task.type]}</span>
                     </td>
-                    <td>Foco</td>
                   </tr>
                 );
               })}
