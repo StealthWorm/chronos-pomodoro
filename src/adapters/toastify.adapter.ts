a536b04eea87fca7bfc535546acae3640d1f2369
Thierry <thierrypitela@hotmail.com>
2025-11-30 09:53:01 -0300

    refactor: fixed toastify adapter to handle confirm option

--- src/adapters/toastify.adapter.ts
+++ src/adapters/toastify.adapter.ts
@@ -1,14 +1,31 @@
 import { toast } from "react-toastify";
+import { Dialog } from "../components/Dialog";
 
-type ToastifyAdapterProps = {
-  message?: string;
-  type: "success" | "warning" | "error" | "info" | "dismiss";
+type ToastifyAdapter = {
+  success: (message?: string) => void;
+  warning: (message?: string) => void;
+  error: (message?: string) => void;
+  info: (message?: string) => void;
+  dismiss: () => void;
+  confirm: (data: string, onClosing: (confirmation: boolean) => void) => ReturnType<typeof toast>;
 }
 
-export const toastifyAdapter: Record<ToastifyAdapterProps["type"], (message?: string) => void> = {
+export const toastifyAdapter: ToastifyAdapter = {
   success: (message?: string) => toast.success(message ?? ""),
   warning: (message?: string) => toast.warning(message ?? ""),
   error: (message?: string) => toast.error(message ?? ""),
   info: (message?: string) => toast.info(message ?? ""),
   dismiss: () => toast.dismiss(),
+  confirm: (data: string, onClosing: (confirmation: boolean) => void) =>
+    toast(Dialog, {
+      data,
+      onClose: confirmation => {
+        if (confirmation) return onClosing(true);
+        return onClosing(false);
+      },
+      autoClose: false,
+      closeOnClick: false,
+      closeButton: false,
+      draggable: false,
+    }),
 }
